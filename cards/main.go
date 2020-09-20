4669c30c632e1307982f8364bb6c58da4d9834dd
Aichi Chang <aichi.p.chang@gmail.com>
2020-09-20 15:21:19 +0100

    init cards

--- cards/main.go
+++ cards/main.go
@@ -4,7 +4,23 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	// is similar to this
-	card := "Ace of Spades"
-	fmt.Println(card)
+	// is similar to this, := allows Go to determin the data type
+	// card := newCard()
+	// fmt.Println(card)
+
+	cards := []string{"Ace if Diamonds", newCard()}
+	// append does not modify the existing slides, it returns a new slices
+	cards = append(cards, "Six of Spades")
+
+	// with for loops in Go,
+	// each loop we need to declear the type of data
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+// when return, we need to tell Go what type of data we want it to return
+func newCard() string {
+	return "Five of Diamonds"
 }
